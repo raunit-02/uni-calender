054a9e48d123aecd17d1cde858d61e039a99697e
Raunit Singh <raunit02@gmail.com>
2022-12-06 22:31:13 +0530

    final commit

--- unicalender/calendarapp/models/__init__.py
+++ unicalender/calendarapp/models/__init__.py
@@ -1,6 +1,5 @@
 from .event_abstract import EventAbstract
 from .event import Event
 from .event_member import EventMember
-from .admin_events import AdminEvents
 
-__all__ = [EventAbstract, Event, EventMember, AdminEvents]
+__all__ = [EventAbstract, Event, EventMember]
--- unicalender/calendarapp/models/admin_events.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class AdminEvents(models.Model):
-    event_name = models.CharField(max_length=200)
-    department = models.CharField(max_length=75)
-    description = models.TextField()
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
